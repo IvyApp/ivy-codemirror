0d8045395155f338868c6e919023ee84e5530f17
Dray Lacy <dray@envylabs.com>
2014-12-12 18:10:51 -0500

    Use minified Bootstrap CSS.

--- Brocfile.js
+++ Brocfile.js
@@ -43,7 +43,6 @@ var app = new EmberAddon({
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
-app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-app.import('bower_components/bootstrap/dist/css/bootstrap.css.map');
+app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
 
 module.exports = app.toTree();
